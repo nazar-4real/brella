f7f01cf4fe6d09bc20f2c327cd93f7d922dd6073
nazar-4real <marusyk.nazar@gmail.com>
2022-12-21 14:53:49 +0200

    updated build

--- src/components/Shared/Logo.jsx
+++ src/components/Shared/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import '../../assets/styles/global.scss';
 
 const Logo = ({ hrefLink = '/', srcPath }) => (
-  <Link className="logo" to={hrefLink} style={{ border: '1px solid red', padding: '7px' }}>
+  <Link className="logo" to={hrefLink}>
     <img className="logo__img" src={srcPath} alt="Logotype" />
   </Link>
 )
